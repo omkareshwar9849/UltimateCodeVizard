d046a4fad8c00f9def15cb656c30fffb569a241a
Omkareshwar <omkar.varada9849@gmail.com>
2024-04-14 20:12:36 +0530

    fix(frontend)

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,21 +1,12 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './header';
-import OtpForm from './OtpForm';
-import CodeDisplay from './codeDisplay';
 import CustomCursor from './CustomCursor';
 import FeaturePlate from'./plate';
 import './home.css';
 import image from "../images/image001.svg";
 
 const Home = () => {
-  // const [otp, setOtp] = useState('');
-  // const [json, setJson] = useState('');
-
-  // const setJsonResponse = (response) => {
-  //   setJson(response);
-  // };
-
   return (
     <div>
       <CustomCursor/>
@@ -40,20 +31,17 @@ const Home = () => {
         <div className="container">
           <strong><h1>FEATURES</h1></strong>
           <div className="feature-list">
-            <FeaturePlate name='codegenerator' title="Code Generator" description=" Automatically generate code snippets or templates based on user inputs or predefined patterns to help developers." />
-            <FeaturePlate name='errorhandler' title="Error Handler" description=" Implement error detection and reporting mechanisms to assist developers in identifying and resolving issues more effectively." />
-            <FeaturePlate name='codereview' title="Code Review" description="Analyze code for adherence to coding standards, best practices, and potential vulnerabilities, providing feedback for improvement." />
-            <FeaturePlate name='timecomplexity' title="Time Complexity" description="Determine the computational efficiency of algorithms by analyzing their time complexity, aiding in performance optimization." />
-            <FeaturePlate name='codeconverter' title="Convertering the Code" description="Translate code from one programming language to another,facilitating interoperability & migration b/w different platforms." />
-            <FeaturePlate name='codeefficiency' title="Improve Code efficiency" description="Offer suggestions and optimizations to enhance the speed, memory usage, and overall performance of code." />
-            <FeaturePlate name='sqlconvert' title="Convertering to the Sql" description="Convert natural language queries or commands into SQL queries for interacting with databases more intuitively." />
+            <FeaturePlate font='gold' name='codegenerator' title="Code Generator" description=" Automatically generate code snippets or templates based on user inputs or predefined patterns to help developers." />
+            <FeaturePlate font='purple' name='errorhandler' title="Error Handler" description=" Implement error detection and reporting mechanisms to assist developers in identifying and resolving issues more effectively." />
+            <FeaturePlate font='gold' name='codereview' title="Code Review" description="Analyze code for adherence to coding standards, best practices, and potential vulnerabilities, providing feedback for improvement." />
+            <FeaturePlate font='gold' name='timecomplexity' title="Time Complexity" description="Determine the computational efficiency of algorithms by analyzing their time complexity, aiding in performance optimization." />
+            <FeaturePlate font='gold' name='codeconverter' title="Convertering the Code" description="Translate code from one programming language to another,facilitating interoperability & migration b/w different platforms." />
+            <FeaturePlate font='gold' name='codeefficiency' title="Improve Code efficiency" description="Offer suggestions and optimizations to enhance the speed, memory usage, and overall performance of code." />
+            <FeaturePlate font='gold' name='sqlconvert' title="Convertering to the Sql" description="Convert natural language queries or commands into SQL queries for interacting with databases more intuitively." />
             
             {/* Add more FeaturePlate components as needed */}
           </div>
         </div>
-        {/* <OtpForm otp={otp} onChange={setOtp} setJson={setJsonResponse} />
-        <h1>Python Code Display</h1>
-        <CodeDisplay code={json.output} /> */}
       </div>
 
     </div>
