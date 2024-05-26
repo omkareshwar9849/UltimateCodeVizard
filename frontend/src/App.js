35a96a690c2b4f81686796496c9ec9e27646165f
Omkareshwar <omkar.varada9849@gmail.com>
2024-05-27 00:34:50 +0530

    feat(backend): initaial backend v2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link,
   Routes
 } from "react-router-dom";
 import './App.css';
-import Home from './components/Home'; // Import the Home component
-import LoginForm from './components/LoginForm';
-import Header from './components/header';
+import Home from './components/Home';
 import Codegenerator from './components/feature/codegenerator';
 import Errorhandler from './components/feature/errorhandler';
 import Codereview from './components/feature/codereview';
@@ -16,23 +13,43 @@ import Timecomplexity from './components/feature/timecomplexity';
 import Codeconverter from './components/feature/codeconverter';
 import Codeefficiency from './components/feature/codeefficiency';
 import Sqlconvert from './components/feature/sqlconvert';
+import CustomCursor from './components/CustomCursor';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const App = () => {
-  
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleColorMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
+
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
   return (
-    <div >
+    <div>
+      <CustomCursor/>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/login" element={<LoginForm />}></Route>
-          <Route exact path="/header" element={<Header />}></Route>
-          <Route exact path="/feature/codegenerator" element={<Codegenerator />}></Route>
-          <Route exact path="/feature/errorhandler" element={<Errorhandler />}></Route>
-          <Route exact path="/feature/codereview" element={<Codereview />}></Route>
-          <Route exact path="/feature/timecomplexity" element={<Timecomplexity />}></Route>
-          <Route exact path="/feature/codeconverter" element={<Codeconverter />}></Route>
-          <Route exact path="/feature/codeefficiency" element={<Codeefficiency />}></Route>
-          <Route exact path="/feature/sqlconvert" element={<Sqlconvert />}></Route>
+          <Route path="/" element={<Home darkMode={darkMode} toggleColorMode={toggleColorMode} />} />
+          <Route path="/login" element={<Login darkMode={darkMode} toggleColorMode={toggleColorMode} alert={alert} showAlert = {showAlert} />} />
+          <Route path="/signup" element={<Signup darkMode={darkMode} toggleColorMode={toggleColorMode} alert={alert} showAlert = {showAlert} />} />
+          <Route path="/feature/codegenerator" element={<Codegenerator darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/errorhandler" element={<Errorhandler darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/codereview" element={<Codereview darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/timecomplexity" element={<Timecomplexity darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/codeconverter" element={<Codeconverter darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/codeefficiency" element={<Codeefficiency darkMode={darkMode} toggleColorMode={toggleColorMode}/>} />
+          <Route path="/feature/sqlconvert" element={<Sqlconvert darkMode={darkMode} toggleColorMode={toggleColorMode} />} />
         </Routes>
       </Router>
     </div>
