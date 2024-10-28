a997b8260bec9e7915e0a04a48c27352dd04c571
omkareshwar9849 <75462615+omkareshwar9849@users.noreply.github.com>
2024-10-28 14:54:09 +0530

    Update Header.jsx, start server on open

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import '../../src/App.css';
 import logo from "../images/logo2.png";
 
 const Header = ({ darkMode, toggleColorMode }) => {
+    const host = process.env.REACT_APP_BACKEND_HOST;
     let navigate = useNavigate();
     const handleLogout = () => {
         localStorage.removeItem('token');
         navigate("/login");
     };
-    
+
+    useEffect(() => {
+
+        const startServer = async () => {
+            const res = await fetch(`${host}/`, {
+                method: "GET"
+              });
+        }
+
+        startServer();
+    }, [])
+
     return (
         <section id="header" >
-            <Link to="/UltimateCodeVizard"><img id='mainLogo' src={logo} alt="" /></Link> {/* Replace anchor tag with Link */}
+            <Link to="/"><img id='mainLogo' src={logo} alt="" /></Link> {/* Replace anchor tag with Link */}
             <div>
                 <ul id="navbar">
                     {/* <button onClick={toggleColorMode} className="btn btn-secondary mx-2">
@@ -24,36 +36,36 @@ const Header = ({ darkMode, toggleColorMode }) => {
                             <Link className="golden-button mx-2" to="/login" role='button'>Login</Link>
                             <Link className="golden-button me-2" to="/signup" role='button'>SignUp</Link>
                             <div>
-                                <input onClick={toggleColorMode} type="checkbox" className="checkbox" id="checkbox" />
-                                <label htmlFor="checkbox" className="checkbox-label">
-                                    <i className="fas fa-moon"></i>
-                                    <i className="fas fa-sun"></i>
-                                    <span className="ball"></span>
+                                <input onClick={toggleColorMode} type="checkbox" class="checkbox" id="checkbox" />
+                                <label for="checkbox" class="checkbox-label">
+                                    <i class="fas fa-moon"></i>
+                                    <i class="fas fa-sun"></i>
+                                    <span class="ball"></span>
                                 </label>
                             </div>
                         </>
                     ) : (
                         <>
-                            <li className='navigators'><Link to="/UltimateCodeVizard" className="active">Home</Link></li>
+                            <li className='navigators'><Link to="/" className="active">Home</Link></li>
                             <li className="dropdown navigators">
                                 <a href="/" className="dropdown-toggle ">Feature</a>
                                 <ul className="dropdown-menu ">
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/codegenerator">Code Generator</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/errorhandler">Error Handler</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/codereview">Code Reviewer</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/timecomplexity">Time Complexity</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/codeconverter">Code Converter</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/codeefficiency">Code Optimizer</Link></li>
-                                    <li className='dropdown-links'><Link to="/UltimateCodeVizard/feature/sqlconvert">Sql Converter</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/codegenerator">Code Generator</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/errorhandler">Error Handler</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/codereview">Code Reviewer</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/timecomplexity">Time Complexity</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/codeconverter">Code Converter</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/codeefficiency">Code Optimizer</Link></li>
+                                    <li className='dropdown-links'><Link to="/feature/sqlconvert">Sql Converter</Link></li>
                                 </ul>
                             </li>
 
                             <div>
-                                <input onClick={toggleColorMode} type="checkbox" className="checkbox" id="checkbox" />
-                                <label htmlFor="checkbox" className="checkbox-label">
-                                    <i className="fas fa-moon"></i>
-                                    <i className="fas fa-sun"></i>
-                                    <span className="ball"></span>
+                                <input onClick={toggleColorMode} type="checkbox" class="checkbox" id="checkbox" />
+                                <label for="checkbox" class="checkbox-label">
+                                    <i class="fas fa-moon"></i>
+                                    <i class="fas fa-sun"></i>
+                                    <span class="ball"></span>
                                 </label>
                             </div>
 
